9f73108abe10bd4732b1e71096fa238a366c9251
LAPTOP-93RQI0HK\surko <surkova-hova@mail.ru>
2021-08-22 22:09:19 +0700

    update ui

--- django_1/riddles/views.py
+++ django_1/riddles/views.py
@@ -5,23 +5,20 @@ from django.shortcuts import render
 import socket
 
 a = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-beg = ''
-end = ''
+beg = None
+end = None
 res = ''
 context = {}
 def index(request):
     global beg, end, a, res, context
-    dict1 = { 1: ('37px', '7px'),
-	2: ('102px', '1px') }
-    d = [i for i in dict1.values()]
-    context = context.update({"chod": d})
-    print(context)
-    if request.POST.get('city') in a:
+    if request.POST.get('city') in a and beg == None:
         beg = request.POST.get('city')
+        res = beg
+        print(beg,end)
         if beg in a:
             context = {"msg": "Введите конечную точку"}
-    elif request.POST.get('end') in a:
-        end = request.POST.get('end')
+    elif request.POST.get('city') in a and beg in a:
+        end = request.POST.get('city')
         context = {"msg": "Ожадание отправки..."}
     elif request.POST.get('err') == 'err':
         end, beg = None, None
